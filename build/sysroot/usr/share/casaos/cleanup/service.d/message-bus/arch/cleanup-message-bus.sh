b1570a599d81d5494536b7389053619135554531
mayumi <mayumi@beesnest-inc.com>
2025-06-10 05:28:20 +0000

    chore: update module name in shell script

--- build/sysroot/usr/share/casaos/cleanup/service.d/message-bus/arch/cleanup-message-bus.sh
+++ build/sysroot/usr/share/casaos/cleanup/service.d/message-bus/arch/cleanup-message-bus.sh
@@ -3,12 +3,12 @@
 set -e
 
 readonly CASA_SERVICES=(
-    "casaos-message-bus.service"
+    "cassetteos-message-bus.service"
 )
 
-readonly CASA_EXEC=casaos-message-bus
-readonly CASA_CONF=/etc/casaos/message-bus.conf
-readonly CASA_DB=/var/lib/casaos/db/message-bus.db
+readonly CASA_EXEC=cassetteos-message-bus
+readonly CASA_CONF=/etc/cassetteos/message-bus.conf
+readonly CASA_DB=/var/lib/cassetteos/db/message-bus.db
 
 readonly aCOLOUR=(
     '\e[38;5;154m' # green  	| Lines, bullets and separators
